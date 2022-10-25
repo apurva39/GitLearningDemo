11fff02fc2df1b387c1041c8015d7f50b33c8e9a
Apurva Anand <apurvaanand101@gmail.com>
2022-10-25 23:04:29 +0530

    vector is added as vector.cpp

---
+++ vector.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> a;
+    for(int i=0;i<a.size();i++)
+    {
+        cin>>a[i];
+        cout<<a[i];
+    }
+    
+    return 0;
+}
